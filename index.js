fbed6884f5d1fb47fd924270fd9a931505ec5c9a
Sumit Saraf <ssaraf@liveperson.com>
2023-01-09 12:21:23 +0530

    new change

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
 const app = express()
+
+// Set up Global configuration access
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,4 +16,45 @@ app.get('/',express.json(),(req,res)=>{
 });
 app.get('/page2',express.json(),(req,res)=>{
     res.sendFile(__dirname+'/page2.html')
+});
+
+
+app.post("/user/generateToken", (req, res) => {
+    // Validate User Here
+    // Then generate JWT Token
+    console.log("generate token")
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+        time: Date(),
+        userId: 12,
+    }
+  
+    const token = jwt.sign(data, jwtSecretKey);
+  
+    res.json(token);
+});
+  
+// Verification of JWT
+app.get("/user/validateToken", (req, res) => {
+    // Tokens are generally passed in header of request
+    // Due to security reasons.
+  
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    // console.log(tokenHeaderKey,jwtSecretKey)
+  
+    try {
+        const token = req.header(tokenHeaderKey);
+        const verified = jwt.verify(token, jwtSecretKey);
+        console.log(verified)
+        if(verified){
+            return res.json({message : "Successfully Verified", data : verified});
+        }else{
+            // Access Denied
+            return res.status(401).send(error);
+        }
+    } catch (error) {
+        // Access Denied
+        return res.status(401).send(error);
+    }
 });
