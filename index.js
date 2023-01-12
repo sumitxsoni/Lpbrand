490ead8dc898585ec9ab7fee84d48f6040c08fd9
Sumit Saraf <ssaraf@liveperson.com>
2023-01-12 17:03:34 +0530

    some changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT , ()=> console.log('server is running on ',PORT))
 
-app.get('/',express.json(),(req,res)=>{
+app.get('/page1',express.json(),(req,res)=>{
     res.sendFile(__dirname+'/page1.html')
 });
 app.get('/page2',express.json(),(req,res)=>{
